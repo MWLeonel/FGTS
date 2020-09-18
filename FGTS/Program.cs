bf7a8983fbc1373d905b36e0c841ae42cd25e372
MWLeonel <wmleonel@gmail.com>
2020-09-18 10:28:34 -0300

    Calcular o FGTS

---
+++ FGTS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+            double FGTS;
+
+            Console.Write("Digite seu Valor Salario R$: ");
+            salario = double.Parse(Console.ReadLine());
+
+            FGTS = (salario / 100) * 8;
+            Console.WriteLine("Seu FGTS é = R${0}", FGTS);
+            Console.Write("\n");
+
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
